56a045928a5266a6a2381c2360c706f5f8c6cbed
Paul Gear <gear54rus@gmail.com>
2017-11-06 13:52:11 +0700

    Webpack now gets the build type from '--env', for #11

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,74 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
-
 const rootDir = resolve(__dirname);
 
-const doNothing = () => {};
+module.exports = function getConfig(environment) {
+  const env = environment || {};
+  const {
+    production,
+  } = env;
 
-module.exports = {
-  context: rootDir,
+  return {
+    context: rootDir,
 
-  entry: [
-    'babel-polyfill',
-    './src/index.js',
-  ],
-
-  output: {
-    path: rootDir + '/dist',
-    filename: 'rested-aps.js',
-  },
-
-  performance: { hints: false },
-
-  // Compiler plugins. See https://github.com/webpack/docs/wiki/list-of-plugins
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: !isProd,
-      options: {
-        eslint: {
-          failOnWarning: isProd,
-          failOnError: isProd,
-        }
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(nodeEnv),
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'eslint-loader',
-        include: resolve(rootDir, 'src'),
-        options: {
-          cache: false,
-        }
-      },
-      {
-        test: /\.js/,
-        include: resolve(rootDir, 'src'),
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: !isProd,
-        }
-      }
-    ]
-  },
-
-  resolve: {
-    modules: [
-      resolve(rootDir, 'src'),
-      'node_modules',
+    entry: [
+      'babel-polyfill',
+      './src/index.js',
     ],
-  }
-}
 
+    output: {
+      path: rootDir + '/dist',
+      filename: 'rested-aps.js',
+    },
+
+    performance: {hints: false},
+    devtool: production ? undefined : 'inline-source-map',
+
+    // Compiler plugins. See https://github.com/webpack/docs/wiki/list-of-plugins
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: !production,
+        options: {
+          eslint: {
+            failOnWarning: production,
+            failOnError: production,
+          }
+        }
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'eslint-loader',
+          include: resolve(rootDir, 'src'),
+          options: {
+            cache: false,
+          }
+        },
+        {
+          test: /\.js/,
+          include: resolve(rootDir, 'src'),
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: !production,
+          }
+        }
+      ]
+    },
+
+    resolve: {
+      modules: [
+        resolve(rootDir, 'src'),
+        'node_modules',
+      ],
+    }
+  }
+};
